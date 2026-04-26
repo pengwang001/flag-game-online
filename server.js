fb51779f61458fcdf58a03e611a711a38f919874
Darrian Wang <darrwang@amazon.com>
2026-04-26 14:15:30 -0700

    Add Local Explorer game mode - WA cities with mixed text+image clues

--- server.js
+++ server.js
@@ -131,6 +131,7 @@ const server = http.createServer((req, res) => {
   else if (req.url === '/countries-110m.json') { file = 'countries-110m.json'; }
   else if (req.url === '/gamebackground.png') { file = 'gamebackground.png'; }
   else if (req.url === '/map') file = 'map.html';
+  else if (req.url === '/local') file = 'local.html';
   else if (req.url === '/qr') {
     const playUrl = `${req.headers['x-forwarded-proto']||'http'}://${req.headers.host}/play`;
     QRCode.toDataURL(playUrl, {width:300,margin:1}, (err,url) => {
